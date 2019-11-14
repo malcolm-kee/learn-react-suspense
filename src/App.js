c5e8a8645029e97f3b65b6d2dacf48fb3e637e75
malcolm-kee <malcolm.keeweesiong@gmail.com>
2019-11-14 10:26:42 +0800

    update react version

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
-import React from 'react';
-import './App.css';
-import { Button } from './components/button';
-import { DadJoke } from './components/dad-joke';
-import { getPokemonResource } from './components/pokemon.data';
-import { getFetchResource } from './lib/ajax';
+import React from "react";
+import "./App.css";
+import { Button } from "./components/button";
+import { getPokemonResource } from "./components/pokemon.data";
 
-const Pokemon = React.lazy(() => import('./components/pokemon'));
-const PokemonToday = React.lazy(() => import('./components/pokemon-today'));
-
-const getJokeResource = () => getFetchResource('https://icanhazdadjoke.com/');
+const Pokemon = React.lazy(() => import("./components/pokemon"));
+const PokemonToday = React.lazy(() => import("./components/pokemon-today"));
 
 function App() {
-  const [jokeResource, setJokeResource] = React.useState(() => getJokeResource());
   const [pokemonId, setPokemonId] = React.useState(0);
   const [pokemonResource, setPokemonResource] = React.useState(null);
   const loadNextPokemon = () => {
@@ -23,17 +18,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Learn React Suspense</h1>
-        <Button onClick={() => setJokeResource(getJokeResource())}>Load Next Joke</Button>
         <Button onClick={loadNextPokemon}>Load Next Pokemon</Button>
       </header>
       <main>
-        <React.Suspense fallback={<span>Loading...</span>}>
-          <DadJoke jokeResource={jokeResource} />
-        </React.Suspense>
         <div
           style={{
-            display: 'flex',
-            justifyContent: 'center',
+            display: "flex",
+            justifyContent: "center",
             margin: `0 auto`,
             maxWidth: 1200
           }}
