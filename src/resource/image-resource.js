b31552422d8d0bb69b3a818ef91fa25df9f4ef68
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2019-11-18 00:07:45 +0800

    add ts

--- src/resource/image-resource.js
+++ src/resource/image-resource.js
@@ -1,5 +1,9 @@
 import { createResource } from './create-resource';
 
+/**
+ *
+ * @param {string} src
+ */
 const loadImage = src => {
   const image = new Image();
   return new Promise((fulfill, reject) => {
